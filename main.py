3b37c8c18401ea6bdda0565dd2979444e475ffc9
hyusa09 <159457146+hyusa09@users.noreply.github.com>
2025-09-04 01:06:34 +0530

    prev

--- main.py
+++ main.py
@@ -568,3 +568,7 @@ if st.session_state.user_role == "university":
 
     if st.button("Save Slot Config"):
         st.success("✅ Slot configuration saved successfully.")
+
+
+elif st.session_state.user_role == "professor":
+    st.title("Professor Portal")
